4977cfa014e08a1961646e095e875f0e6fc95d46
David M <uberl33thax0r@hotmail.com>
2014-09-10 03:31:22 -0400

    Allow chapter and section processing to be run independantly. Remove chapter processing from default actions

--- ppprep.py
+++ ppprep.py
@@ -3,8 +3,8 @@
 """ppprep
 
 Usage:
-  ppprep [-abcip] <infile>
-  ppprep [-abcip] <infile> <outfile>
+  ppprep [-abceip] <infile>
+  ppprep [-abceip] <infile> <outfile>
   ppprep -h | --help
   ppprep -v | --version
 
@@ -15,13 +15,14 @@ Examples:
   ppprep school-src.txt school2-src.txt
 
 Options:
-  -h --help             Show help.
-  -v --version          Show version.
-  -i --illustrations    Convert [Illustration] tags into ppgen .il/.ca markup 
-  -p --pagenumbers      Convert page breaks into ppgen // 001.png style 
-  -b --blankpages       Comment out [Blank Page] lines
-  -c --headings         Convert chapter and section headings into ppgen style chapter and section headings
-  -a --all              Perform all actions (default)
+  -h --help           Show help.
+  -v --version        Show version.
+  -a --all            Perform all safe actions. (-bip) (default)
+  -b --blankpages     Comment out [Blank Page] lines.
+  -c --chapters       Convert chapter headings into ppgen style chapter headings.
+  -e --sections       Convert section headings into ppgen style section headings.
+  -i --illustrations  Convert [Illustration] tags into ppgen .il/.ca markup.
+  -p --pagenumbers    Convert page breaks into ppgen // 001.png style.
 """  
 
 from docopt import docopt
@@ -113,10 +114,11 @@ def findNextNonEmptyLine( buf, startLine ):
 	
 	return lineNum
 	
-def processHeadings( inBuf ):
+def processHeadings( inBuf, doChapterHeadings, doSectionHeadings ):
 	outBuf = []
 	lineNum = 0 
 	consecutiveEmptyLineCount = 0
+	rewrapLevel = 0
 	foundChapterHeadingStart = False
 
 	while lineNum < len(inBuf):
@@ -124,13 +126,28 @@ def processHeadings( inBuf ):
 		# Chapter heading blocks are in the form:
 			# (4 empty lines)
 			# chapter name
+			# can span more than one line
 			# (1 empty line)
 			# chapter description, opening quote, etc., 1 empty line seperating each
 			# ...
 			# (2 empty lines)
+
+		# Section heading blocks are in the form
+			# (2 empty lines)
+			# section name
+			# can span more than one line
+			# (1 empty line)
+
+#		print("** " + str(consecutiveEmptyLineCount) + ":" + str(rewrapLevel) + " : " + inBuf[lineNum])
 		
+		# Out-of-line formatting /# #/ /* */
+		if( re.match(r"^\/\*$", inBuf[lineNum]) or re.match(r"^\/\#$", inBuf[lineNum]) ):
+			rewrapLevel += 1
+		elif( re.match(r"^\*\/$", inBuf[lineNum]) or re.match(r"^\#\/$", inBuf[lineNum]) ):
+			rewrapLevel -= 1
+			
 		# Chapter heading
-		if( consecutiveEmptyLineCount == 4 and not isLineBlank(inBuf[lineNum]) ):
+		if( doChapterHeadings and consecutiveEmptyLineCount == 4 and not isLineBlank(inBuf[lineNum]) and rewrapLevel == 0 ):
 			inBlock = []
 			outBlock = []
 			foundChapterHeadingEnd = False;
@@ -156,56 +173,92 @@ def processHeadings( inBuf ):
 					inBlock.append(inBuf[lineNum])
 					lineNum += 1
 			
-			# Title pages look like chapter headings but start with /*, handle it
-			if( re.match(r"^\/\*$", inBlock[0]) ):
-#               print("********* FALSE HIT, NOT A CHAPTER HEADING *********")
-				for line in inBlock:
-					outBlock.append(line)
+			# Remove the four consecutive blank lines that preceeds chapter heading
+			outBuf = outBuf[:-4]
+		
+			# .sp 4
+			# .h2 id=chapter_vi
+			# CHAPTER VI.||chapter description etc..
+			# .sp 2             
+			chapterID = formatAsID(inBlock[0])
+			chapterLine = ""
+			for line in inBlock:
+				chapterLine += line
+				chapterLine += "|"
+			chapterLine = chapterLine[:-1]
 			
-			# Convert chapter heading to ppgen format
-			else:
-				# Remove three of the four consecutive blank lines from output buf
-				outBuf = outBuf[:-4]
-			
-				# .sp 4
-				# .h2 id=chapter_vi
-				# CHAPTER VI.||chapter description etc..
-				# .sp 2             
-				chapterID = formatAsID(inBlock[0])
-				chapterLine = ""
-				for line in inBlock:
-					chapterLine += line
-					chapterLine += "|"
-				chapterLine = chapterLine[:-1]
-				
-				outBlock.append("// ******** PPPREP GENERATED **************************************") 
-				outBlock.append(".sp 4")
-				outBlock.append(".h2 id=" + chapterID )             
-				outBlock.append(chapterLine)
-				outBlock.append(".sp 2")
+			outBlock.append("// ******** PPPREP GENERATED ****************************************") 
+			outBlock.append(".sp 4")
+			outBlock.append(".h2 id=" + chapterID )             
+			outBlock.append(chapterLine)
+			outBlock.append(".sp 2")
 
-				# Write out original as a comment
-				outBlock.append(".ig  // *** PPPREP BEGIN ORIGINAL *********************************") 
-				outBlock.append("") 
-				outBlock.append("") 
-				outBlock.append("") 
-				outBlock.append("") 
-				for line in inBlock:
-					outBlock.append(line)
-				outBlock.append(".ig- // *** END ***************************************************")
-					
+			# Write out original as a comment
+			outBlock.append(".ig  // *** PPPREP BEGIN ORIGINAL ***********************************") 
+			outBlock.append("") 
+			outBlock.append("") 
+			outBlock.append("") 
+			outBlock.append("") 
+			for line in inBlock:
+				outBlock.append(line)
+			outBlock.append(".ig- // *** END *****************************************************")
+				
 			# Write out chapter heading block
 			for line in outBlock:
 				outBuf.append(line)
+			
+			# Log action
+			print(".h2 " + chapterLine)
 				
 		# Section heading
-#       elif( consecutiveEmptyLineCount == 2 and not isLineBlank(inBuf[lineNum]) ):
-#           print("*** FOUND SECTION HEADING ***")
-#           print(outBuf[lineNum:-2])
-#           print(outBuf[lineNum:-1])
-#           print(outBuf[lineNum])
-#           print("*****************************") 
+		elif( doSectionHeadings and consecutiveEmptyLineCount == 2 and not isLineBlank(inBuf[lineNum]) and rewrapLevel == 0 ):
+			inBlock = []
+			outBlock = []
+			foundSectionHeadingEnd = False;
+			consecutiveEmptyLineCount = 0;
+		
+			# Copy section heading block to inBlock
+			while( lineNum < len(inBuf) and not foundSectionHeadingEnd ):
+				if( isLineBlank(inBuf[lineNum]) ):
+					foundSectionHeadingEnd = True
+				else:
+					inBlock.append(inBuf[lineNum])
+					lineNum += 1
 			
+			# Remove two consecutive blank lines that preceed section heading
+			outBuf = outBuf[:-2]
+		
+			# .sp 2
+			# .h3 id=section_i
+			# Section I.
+			# .sp 1             
+			sectionID = formatAsID(inBlock[0])
+			sectionLine = ""
+			for line in inBlock:
+				sectionLine += line
+				sectionLine += "|"
+			sectionLine = sectionLine[:-1]
+			
+			outBlock.append("// ******** PPPREP GENERATED ****************************************") 
+			outBlock.append(".sp 2")
+			outBlock.append(".h3 id=" + sectionID )             
+			outBlock.append(sectionLine)
+			outBlock.append(".sp 1")
+
+			# Write out original as a comment
+			outBlock.append(".ig  // *** PPPREP BEGIN ORIGINAL ***********************************") 
+			outBlock.append("") 
+			outBlock.append("") 
+			for line in inBlock:
+				outBlock.append(line)
+			outBlock.append(".ig- // *** END *****************************************************")
+				
+			# Write out chapter heading block
+			for line in outBlock:
+				outBuf.append(line)
+			
+			# Log action
+			print("  .h3 " + sectionID)
 		else:
 			if( isLineBlank(inBuf[lineNum]) ):
 				consecutiveEmptyLineCount += 1
@@ -231,8 +284,7 @@ def processIllustrations( inBuf ):
 			img = Image.open(f)
 			img.load()
 		except:
-			print("Unable to load image", f)
-			sys.exit(1)
+			fatal("Unable to load image: " + f)
 
 #       print(f, img.size);     
 		m = re.match(r"images/i_([^\.]+)", f)
@@ -283,7 +335,11 @@ def processIllustrations( inBuf ):
 			# Convert to ppgen illustration block
 #           .il id=i_001 fn=i_001.jpg w=600 alt=''
 #           .ca SOUTHAMPTON BAR IN THE OLDEN TIME.
-			outBlock.append( ".il id=i" + currentScanPage + " fn=" +  illustrations[currentScanPage]['fileName'] + " w=" + str(illustrations[currentScanPage]['dimensions'][0]) + " alt=''" )
+			try:
+				outBlock.append( ".il id=i" + currentScanPage + " fn=" +  illustrations[currentScanPage]['fileName'] + " w=" + str(illustrations[currentScanPage]['dimensions'][0]) + " alt=''" )
+			except KeyError:
+				fatal("No image file for illustration located on scan page " + currentScanPage + ".png");
+			
 			captionLine = ""
 			for line in inBlock:
 				line = re.sub(r"^\[Illustration: ", "", line)
@@ -303,8 +359,8 @@ def processIllustrations( inBuf ):
 				outBuf.append(line)
 				
 			# Write out boilerplate code for HTML version as comment in case .il is not sufficient
-			outBuf.append(".ig  // *** PPPREP BEGIN ************************************************************")
-			outBuf.append("// ******** Alternative inline HTML version for use when .il .ca are insufficient ***") 
+			outBuf.append(".ig  // *** PPPREP BEGIN **************************************************************")
+			outBuf.append("// ******** Alternative inline HTML version for use when .il .ca are insufficient *****") 
 			outBuf.append(".if h")
 			outBuf.append(".de .customCSS { clear:left; float:left; margin:4% 4% 4% 0; }")
 			outBuf.append(".li")
@@ -317,7 +373,7 @@ def processIllustrations( inBuf ):
 			for line in inBlock:
 				outBuf.append(line)
 			outBuf.append(".if-")           
-			outBuf.append(".ig- // *** END *********************************************************************")
+			outBuf.append(".ig- // *** END ***********************************************************************")
 			
 		else:
 			outBuf.append(inBuf[lineNum])
@@ -361,7 +417,7 @@ def loadFile(fn):
 			pass
 
 	if encoding == "":
-		self.fatal("cannot determine input file decoding")
+		fatal("cannot determine input file decoding")
 	else:
 		# self.info("input file is: {}".format(encoding))
 		if encoding == "ASCII":
@@ -384,63 +440,65 @@ def warn(message):
 		sys.stderr.write("**warning: " + message + "\n")
 
 
+def createOutputFileName( infile ):
+	outfile = infile.split('.')[0] + "-src.txt"
+	return outfile
+
 def main():
-	try:
-		args = arguments = docopt(__doc__, version='ppprep 0.1')
-		print(args)
+	args = docopt(__doc__, version='ppprep 0.1')
+#		print(args)
 
-		# Process required command line arguments
-		outfile = "out.txt"
-		if( args['<outfile>'] ):
-			outfile = args['<outfile>']
+	# Process required command line arguments
+	outfile = createOutputFileName( args['<infile>'] )
+	if( args['<outfile>'] ):
+		outfile = args['<outfile>']
+		
+	infile = args['<infile>']
+
+	# Open source file and represent as an array of lines
+	inBuf = loadFile( infile )
+
+	# Process optional command line arguments
+	doBlankPages = args['--blankpages'];
+	doChapterHeadings = args['--chapters'];
+	doSectionHeadings = args['--sections'];
+	doIllustrations = args['--illustrations'];
+	doPageNumbers = args['--pagenumbers'];
+	
+	# Default to --all if no other options set
+	if( not doBlankPages and \
+		not doChapterHeadings and \
+		not doSectionHeadings and \
+		not doIllustrations and \
+		not doPageNumbers or \
+		args['--all'] ):
+		doBlankPages = True;
+		doIllustrations = True;
+		doPageNumbers = True;
 			
-		infile = args['<infile>']
+	# Process source document
+	outBuf = []
+	if( doChapterHeadings or doSectionHeadings ):
+		outBuf = processHeadings( inBuf, doChapterHeadings, doSectionHeadings )
+		outBuf = processHeadings( inBuf, doChapterHeadings, doSectionHeadings )
+		inBuf = outBuf
+	if( doBlankPages ):
+		outBuf = processBlankPages( inBuf )
+		inBuf = outBuf
+	if( doPageNumbers or doIllustrations ):
+		outBuf = processPageNumbers( inBuf )
+		inBuf = outBuf
+	if( doIllustrations ):
+		outBuf = processIllustrations( inBuf )
+		inBuf = outBuf
 
-		# Open source file and represent as an array of lines
-		inBuf = loadFile( infile )
-
-		# Process optional command line arguments
-		doPageNumbers = args['--pagenumbers'];
-		doBlankPages = args['--blankpages'];
-		doIllustrations = args['--illustrations'];
-		doHeadings = args['--headings'];
-		
-		# Default to --all if no other options set
-		if( not doPageNumbers and \
-			not doBlankPages and \
-			not doIllustrations and \
-			not doHeadings or \
-			args['--all'] ):
-			doPageNumbers = True;
-			doBlankPages = True;
-			doIllustrations = True;
-			doHeadings = True;
-				
-		# Process source document
-		outBuf = []
-		if( doHeadings ):
-			outBuf = processHeadings( inBuf )
-			inBuf = outBuf
-		if( doBlankPages ):
-			outBuf = processBlankPages( inBuf )
-			inBuf = outBuf
-		if( doPageNumbers or doIllustrations ):
-			outBuf = processPageNumbers( inBuf )
-			inBuf = outBuf
-		if( doIllustrations ):
-			outBuf = processIllustrations( inBuf )
-			inBuf = outBuf
-
-		# Save file
-		f = open(outfile,'w')
-		for line in outBuf:
-			f.write(line+'\n')
-		f.close()
-		
-	except docopt.DocoptExit as e:
-		print(e.message)
-		
-		return
+	# Save file
+	f = open(outfile,'w')
+	for line in outBuf:
+		f.write(line+'\n')
+	f.close()
+	
+	return
 
 
 if __name__ == "__main__":
