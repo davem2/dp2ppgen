e3a9aff30ff07bbe72afb6caeb058c860174387c
David Maranhao <uberl33thax0r@hotmail.com>
2015-03-11 21:56:29 -0400

    Restructure project for packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dp2ppgen',
+  packages = ['dp2ppgen'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Translates pgdp.org formatted text files into ppgen syntax.',
+  author = 'David Maranhao',
+  author_email = 'david.maranhao@gmail.com',
+  url = 'https://github.com/davem2/dp2ppgen', # use the URL to the github repo
+  download_url = 'https://github.com/davem2/dp2ppgen/tarball/0.1', # I'll explain this in a second
+  keywords = ['text', 'processing', 'books', 'gutenberg', 'distributedproofers'], # arbitrary keywords
+  classifiers = [],
+)
