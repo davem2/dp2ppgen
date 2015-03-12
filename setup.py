42189286b4303d43c6e95362605b8455141adbb8
David Maranhao <uberl33thax0r@hotmail.com>
2015-03-12 16:46:08 -0400

    Testing cross platform install

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(
-  name = 'dp2ppgen',
-  packages = ['dp2ppgen'], # this must be the same as the name above
-  version = '0.1',
-  description = 'Translates pgdp.org formatted text files into ppgen syntax.',
-  author = 'David Maranhao',
-  author_email = 'david.maranhao@gmail.com',
-  url = 'https://github.com/davem2/dp2ppgen', # use the URL to the github repo
-  download_url = 'https://github.com/davem2/dp2ppgen/tarball/0.1', # I'll explain this in a second
-  keywords = ['text', 'processing', 'book', 'gutenberg', 'distributedproofreaders'], # arbitrary keywords
-  classifiers = [],
+  name          = 'dp2ppgen',
+  author        = 'David Maranhao',
+  author_email  = 'david.maranhao@gmail.com',
+  license       = 'GPL3',
+  description   = 'Translates pgdp.org formatted text files into ppgen syntax.',
+  packages      = ['dp2ppgen'], # this must be the same as the name above
+  version       = '0.1',
+  url           = 'https://github.com/davem2/dp2ppgen', # use the URL to the github repo
+  download_url  = 'https://github.com/davem2/dp2ppgen/tarball/0.1', # I'll explain this in a second
+  keywords      = ['text', 'processing', 'book', 'gutenberg', 'distributedproofreaders'], # arbitrary keywords
+  entry_points = {
+      'console_scripts': [
+          'dp2ppgen = dp2ppgen.dp2ppgen:main',
+      ],
+  },
+  install_requires = [
+    'docopt >= 0.6.1',
+    'docutils >= 0.12',
+  ]
 )
+
